60d52faeb9e7fa835b61c2afbb1ac8d7bd13d455
digitopia01 <140796008+digitopia01@users.noreply.github.com>
2023-08-30 17:48:08 +0900

    재업
    재업

--- js/main.js
+++ js/main.js
@@ -18,3 +18,48 @@ flipping.onmouseout = () => {
   todevback.style.display = "inline-block";
   todevback.style.opacity = "0";
 };
+
+let topbutton = document.querySelector(".top"),
+  nav = document.querySelectorAll(".navA");
+
+function navcolor() {
+  for (i = 0; i < nav.length; i++) {
+    nav[i].onmouseover = () => {
+      nav[i].style.color = "rgb(43, 198, 226);";
+    };
+  }
+}
+
+navcolor();
+
+nav[1].onclick = () => {
+  window.scrollTo({
+    top: 1000,
+  });
+};
+
+window.onscroll = () => {
+  let scroll = scrollY;
+  console.log(scroll);
+
+  if (scroll >= 50) {
+    topbutton.style.transform = "translateY(0px)";
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth",
+      duration: 300,
+    });
+  } else {
+    topbutton.style.transform = "translateY(100px)";
+  }
+
+  if (scroll > 175 && scroll < 1300) {
+    for (i = 0; i < nav.length; i++) {
+      nav[i].style.color = "#333";
+    }
+  } else {
+    for (i = 0; i < nav.length; i++) {
+      nav[i].style.color = "#fff";
+    }
+  }
+};
